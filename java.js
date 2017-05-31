28938ad6a2f069c92b8b115e3d4f42df704dd8ab
Pål Storsveen <paal@innit.no>
2017-05-31 15:24:55 +0200

    added animations

--- java.js
+++ java.js
@@ -3,6 +3,7 @@
  */
 var arr = new Array();
 $('.next-ball').on('click', function(){
+
 	var random = Math.floor(Math.random()*100);
 	console.log(random);
 	if( arr.indexOf(random) < 1){
@@ -13,15 +14,47 @@ $('.next-ball').on('click', function(){
 	}
 	console.log(arr);
 	if ( $('.lottery').children().length > 5 ) {
-		$('.next-ball').hide();
-		$('.play-again').show();
-	}
 
+		$('.next-ball').addClass('bounceOutLeft');
+		$('.next-ball').addClass('animated');
+
+		setTimeout(function(){
+			$('.next-ball').hide();
+			$('.play-again').removeClass('bounceOutRight');
+		}, 1000);
+
+
+		$('.play-again').show();
+
+
+		setTimeout(function () {
+			$('.next-ball').removeClass('bounceOutLeft');
+			$('.next-ball').removeClass('animated');
+		}, 1000);
+
+		$('.play-again').addClass('bounceInRight');
+		$('.play-again').addClass('animated');
+
+	}
 
 });
 $('.play-again').on('click', function(){
+	//bouncing out the play-again button
+	$('.play-again').removeClass('bounceInRight');
+	$('.play-again').addClass('bounceOutRight');
+
+	//waits 2 seconds before hiding play-again button
+	setTimeout(function(){
+		$('.play-again').hide();
+		$('.play-again').removeClass('bounceOutRight');
+	}, 2000);
+
 	$('.lottery').children().remove();
 	arr = [];
+
 	$('.next-ball').show();
-	$('.play-again').hide();
+	$('.next-ball').addClass('bounceInLeft');
+	$('.next-ball').addClass('animated');
+
+
 });
